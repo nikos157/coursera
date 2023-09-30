e57e25c936400e695c758b100c9b9350606b7a7b
nikos157 <nikolaigreekman@windowslive.com>
2023-09-30 22:31:04 +0300

    update

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -8,14 +8,14 @@ import "../App.css";
 function Footer() {
     return (
         <footer>
-            <a href="#"><img src={logo} alt="logo" /></a>
+            <a href="#" style={{marginLeft:"20px",marginTop:"10px"}}><img src={logo} alt="logo" /></a>
             <ul>
                 <li><a href='#'><FontAwesomeIcon icon={faFacebook}/></a></li>
                 <li><a href='#'><FontAwesomeIcon icon={faInstagram} /></a></li>
                 <li><a href='#'><FontAwesomeIcon icon={faYoutube} /></a></li>
                 <li><a href='#'><FontAwesomeIcon icon={faTwitter} /></a></li>
             </ul>
-            <p>&copy;2023 little lemon</p>
+            <p style={{ marginLeft: "20px" }}>&copy;2023 little lemon</p>
         </footer>
     )
 }
