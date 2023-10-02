f1f29942eec81502ce1614a3c6e498adad318494
nikos157 <nikolaigreekman@windowslive.com>
2023-10-03 02:15:08 +0300

    css fixes

--- src/component/BookingPage.js
+++ src/component/BookingPage.js
@@ -2,7 +2,7 @@ import BookingForm from "./BookingForm"
 
 const BookingPage=()=>{
    return (
-    <div style={{display: "grid",gridTemplateColumns: "1fr 1fr",height: "100vh"}}>
+    <div id="reserve" >
         <div style={{textAlign: "center", width: "300px", gap:"0", margin:"auto auto auto auto"}}>
             <h1>Reserve A Table</h1>
             <p>Reserve a table now in order to experience our mediteran cuisine first hand</p>
