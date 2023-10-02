c03a3f1504bd6890600af719e8ef1d77786e1f8b
nikos157 <nikolaigreekman@windowslive.com>
2023-10-02 21:54:18 +0300

    update

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,15 +1,27 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Main from "./Main";
 function Nav() {
     return (
+        <>
         <nav>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Menu</a></li>
-                <li><a href="#">Reservations</a></li>
-                <li><a href="#">Order Online</a></li>
-                <li><a href="#">login</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/">About</Link></li>
+                <li><Link to="/">Menu</Link></li>
+                <li><Link to="/">Reservations</Link></li>
+                <li><Link to="/">Order Online</Link></li>
+                <li><Link to="/">login</Link></li>
             </ul>
         </nav>
+        <Routes>
+            <Route path="" element={Main}></Route>
+            <Route path="" element={Main}></Route>
+            <Route path="" element={Main}></Route>
+            <Route path="" element={Main}></Route>
+            <Route path="" element={Main}></Route>
+            <Route path="" element={Main}></Route>
+        </Routes>
+        </>
     )
 }
 export default Nav
