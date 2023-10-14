b2d970138dcc5d8ebf506d6916a73befd4664a51
nikos157 <nikolaigreekman@windowslive.com>
2023-10-14 20:40:08 +0300

    update

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,7 +1,7 @@
 function Card(props) {
     return (
-        <div style={{width: "25vw",height:"fit-content",backgroundColor:"white",minWidth:"300px", justifySelf:"center"}}>
-            <img src={props.src} alt="card photo" style={{width:"25vw", minWidth:"300px"}}/>
+        <div className="card" style={{width: "25vw",height:"fit-content",backgroundColor:"white",minWidth:"300px", justifySelf:"center"}}>
+            <img src={props.src} alt="card" style={{width:"25vw", minWidth:"300px"}}/>
             <h2>{props.title}</h2>
             <p>{props.par}</p>
         </div>
